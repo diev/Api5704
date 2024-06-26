23ca3882d553cd99be3198182afb4dd563f1806a
Dmitrii Evdokimov <diev@mail.ru>
2024-06-26 03:59:28 +0300

    Update project

---
+++ build.cmd
@@ -0,0 +1,73 @@
+@echo off
+set src=Api5704
+
+for %%i in (.) do (
+ set repo=%%~nxi
+)
+
+for %%i in (%src%\*.csproj) do (
+ set prj=%%~dpnxi
+ set app=%%~ni
+)
+
+rd /s /q bin
+
+rem Build an app with many dlls (default)
+rem dotnet publish %prj% -o bin
+
+rem Build a single-file app when NET Desktop runtime required 
+dotnet publish %prj% -o bin -r win-x64 -p:PublishSingleFile=true --no-self-contained
+
+rem Build a single-file app when no runtime required
+rem dotnet publish %prj% -o bin -r win-x64 -p:PublishSingleFile=true
+
+for /f "tokens=3 delims=<>" %%v in ('findstr "<Version>" %prj%') do set version=%%v
+for /f "tokens=3 delims=<>" %%v in ('findstr "<Description>" %prj%') do set description=%%v
+
+for /f "tokens=3,3" %%a in ('reg query "hkcu\control panel\international" /v sshortdate') do set sfmt=%%a
+for /f "tokens=3,3" %%a in ('reg query "hkcu\control panel\international" /v slongdate') do set lfmt=%%a
+
+reg add "hkcu\control panel\international" /v sshortdate /t reg_sz /d yyyy-MM-dd /f >nul
+reg add "hkcu\control panel\international" /v slongdate /t reg_sz /d yyyy-MM-dd /f >nul
+
+set ymd=%date%
+
+reg add "hkcu\control panel\international" /v sshortdate /t reg_sz /d %sfmt% /f >nul
+reg add "hkcu\control panel\international" /v slongdate /t reg_sz /d %lfmt% /f >nul
+
+set pack=%app%-v%version%.zip
+if exist %pack% del %pack%
+
+call :version_txt > bin\version.txt
+
+"C:\Program Files\7-Zip\7z.exe" a %pack% *.md *.sln *.cmd bin\ Templates\ XLSM\
+"C:\Program Files\7-Zip\7z.exe" a %pack% -x!.* -xr!bin -xr!obj -xr!PublishProfiles -xr!*.user %src%\
+
+set store=G:\BankApps\AppStore
+if exist %store% copy /y %pack% %store%
+
+endlocal
+exit /b 0
+
+:lower
+echo>%Temp%\%2
+for /f %%f in ('dir /b/l %Temp%\%2') do set %1=%%f
+del %Temp%\%2
+goto :eof
+
+:version_txt
+call :lower repol %repo%
+echo %app%
+echo %description%
+echo.
+echo Version: v%version%
+echo Date:    %ymd%
+echo.
+echo Requires SDK .NET 8.0 to build
+echo Requires .NET Desktop Runtime 8.0 to run
+echo Download from https://dotnet.microsoft.com/download
+echo.
+echo https://github.com/diev/%repo%
+echo https://gitverse.ru/diev/%repo%
+echo https://gitflic.ru/project/diev/%repol%
+goto :eof
